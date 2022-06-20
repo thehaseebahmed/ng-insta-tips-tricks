5ed9ae514aa9855d64f1bf4a25b7ca0314dae3b9
thehaseebahmed <thehaseebahmed@outlook.com>
2022-06-20 11:41:51 +0500

    Aspect Ratio Images - Initial

---
+++ src/app/aspect-ratio-images/aspect-ratio-images.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tha-aspect-ratio-images',
+  templateUrl: './aspect-ratio-images.component.html',
+  styleUrls: ['./aspect-ratio-images.component.css'],
+})
+export class ThaAspectRatioImagesComponent {}
