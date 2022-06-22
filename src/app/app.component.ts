3aa0c6b1da44243446d8cc3171554d979927027b
thehaseebahmed <thehaseebahmed@outlook.com>
2022-06-22 11:41:02 +0500

    Minor Layout Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'tha-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  public readonly components: Array<any>;
+  public selectedComponent: any;
+
+  constructor() {
+    this.components = [
+      {
+        title: 'Aspect Ratio Images',
+        description: 'This is its description.',
+        route: '',
+      },
+    ];
+  }
+
+  ngOnInit(): void {
+    this.selectedComponent = this.components[0];
+  }
 }
