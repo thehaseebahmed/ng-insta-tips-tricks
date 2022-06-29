a45f857ee00290b6033632f86543e3268e5a2937
thehaseebahmed <thehaseebahmed@outlook.com>
2022-06-29 15:29:07 +0500

    Initial Work

---
+++ src/app/flip-card/flip-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tha-flip-card',
+  templateUrl: './flip-card.component.html',
+  styleUrls: ['./flip-card.component.css'],
+})
+export class ThaFlipCardComponent {}
