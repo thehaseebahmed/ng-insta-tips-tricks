5324c7dd8063f8e386221c32d3d1feb80b5dcc69
thehaseebahmed <thehaseebahmed@outlook.com>
2022-07-06 10:44:17 +0500

    Added Instagram ID to Vertical Align

--- src/app/vertical-align/vertical-align.component.ts
+++ src/app/vertical-align/vertical-align.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./vertical-align.component.css'],
 })
 export class ThaVerticalAlignComponent {
-  private instagramId: string | undefined = undefined;
+  private instagramId: string | undefined = "CfniynVDy9q";
   public url: string = this.instagramId
     ? `https://www.instagram.com/p/${this.instagramId}/?utm_source=stackblitz`
     : 'https://www.instagram.com/thasdevlife?utm_source=stackblitz';
